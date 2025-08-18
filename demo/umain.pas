7e9a11aba91b2a86514c0d5b1964f97b374bb027
Morgunov Evgeny <104060685+EvgeniyMorgan@users.noreply.github.com>
2025-08-18 16:25:19 +0300

    Add files via upload

---
+++ demo/umain.pas
@@ -0,0 +1,32 @@
+unit umain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, meHotKey;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    meHotKey1: TmeHotKey;
+    meHotKey2: TmeHotKey;
+    meHotKey3: TmeHotKey;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
